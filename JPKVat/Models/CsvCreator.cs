327a0f7cf0e65d5d3931b9454af0b5338f204dc6
ChangLi80 <helpdesk@exp-suite.com>
2018-02-27 13:12:36 +0100

    2018.02.27

--- JPKVat/Models/CsvCreator.cs
+++ JPKVat/Models/CsvCreator.cs
@@ -12,7 +12,9 @@ namespace JPKVat.Models
 {
     public class CsvCreator
     {
-        public readonly string header = "KodFormularza;kodSystemowy;wersjaSchemy;WariantFormularza;CelZlozenia;DataWytworzeniaJPK;DataOd;DataDo;DomyslnyKodWaluty;KodUrzedu;NIP;PelnaNazwa;REGON;KodKraju;Wojewodztwo;Powiat;Gmina;Ulica;NrDomu;NrLokalu;Miejscowosc;KodPocztowy;Poczta;typSprzedazy;LpSprzedazy;NrKontrahenta;NazwaKontrahenta;AdresKontrahenta;DowodSprzedazy;DataWystawienia;DataSprzedazy;K_10;K_11;K_12;K_13;K_14;K_15;K_16;K_17;K_18;K_19;K_20;K_21;K_22;K_23;K_24;K_25;K_26;K_27;K_28;K_29;K_30;K_31;K_32;K_33;K_34;K_35;K_36;K_37;K_38;K_39;LiczbaWierszySprzedazy;PodatekNalezny;typZakupu;LpZakupu;NrDostawcy;NazwaDostawcy;AdresDostawcy;DowodZakupu;DataZakupu;DataWplywu;K_43;K_44;K_45;K_46;K_47;K_48;K_49;K_50;LiczbaWierszyZakupow;PodatekNaliczony";
+        //public readonly string header = "KodFormularza;kodSystemowy;wersjaSchemy;WariantFormularza;CelZlozenia;DataWytworzeniaJPK;DataOd;DataDo;DomyslnyKodWaluty;KodUrzedu;NIP;PelnaNazwa;REGON;KodKraju;Wojewodztwo;Powiat;Gmina;Ulica;NrDomu;NrLokalu;Miejscowosc;KodPocztowy;Poczta;typSprzedazy;LpSprzedazy;NrKontrahenta;NazwaKontrahenta;AdresKontrahenta;DowodSprzedazy;DataWystawienia;DataSprzedazy;K_10;K_11;K_12;K_13;K_14;K_15;K_16;K_17;K_18;K_19;K_20;K_21;K_22;K_23;K_24;K_25;K_26;K_27;K_28;K_29;K_30;K_31;K_32;K_33;K_34;K_35;K_36;K_37;K_38;K_39;LiczbaWierszySprzedazy;PodatekNalezny;typZakupu;LpZakupu;NrDostawcy;NazwaDostawcy;AdresDostawcy;DowodZakupu;DataZakupu;DataWplywu;K_43;K_44;K_45;K_46;K_47;K_48;K_49;K_50;LiczbaWierszyZakupow;PodatekNaliczony";
+
+        public readonly string header =   "KodFormularza;kodSystemowy;wersjaSchemy;WariantFormularza;CelZlozenia;DataWytworzeniaJPK;DataOd;DataDo;NazwaSystemu;NIP;PelnaNazwa;Email;LpSprzedazy;NrKontrahenta;NazwaKontrahenta;AdresKontrahenta;DowodSprzedazy;DataWystawienia;DataSprzedazy;K_10;K_11;K_12;K_13;K_14;K_15;K_16;K_17;K_18;K_19;K_20;K_21;K_22;K_23;K_24;K_25;K_26;K_27;K_28;K_29;K_30;K_31;K_32;K_33;K_34;K_35;K_36;K_37;K_38;K_39;LiczbaWierszySprzedazy;PodatekNalezny;LpZakupu;NrDostawcy;NazwaDostawcy;AdresDostawcy;DowodZakupu;DataZakupu;DataWplywu;K_43;K_44;K_45;K_46;K_47;K_48;K_49;K_50;LiczbaWierszyZakupow;PodatekNaliczony";
 
         public bool CreateCSvFile(Ijpk jpk)
         {
@@ -39,31 +41,31 @@ namespace JPKVat.Models
                     csv.WriteField(jpk.DataWytworzeniaJPK.ToJPKDataUtworzenia());
                     csv.WriteField(jpk.DataOd.ToJPK());
                     csv.WriteField(jpk.DataDo.ToJPK());
-                    csv.WriteField(jpk.DomyslnyKodWaluty);
-                    csv.WriteField(jpk.KodUrzedu);
-                    for (int i = 0; i < fields.Count() - fields.ToList().IndexOf("KodUrzedu"); i++) csv.WriteField("");
+                    csv.WriteField("My JPK");
+                    //csv.WriteField(jpk.KodUrzedu);
+                    for (int i = 0; i < fields.Count() - fields.ToList().IndexOf("NazwaSystemu"); i++) csv.WriteField("");
                     csv.NextRecord();
 
                     for (int i = 0; i < fields.ToList().IndexOf("NIP"); i++) csv.WriteField("");
                     csv.WriteField(jpk.NIP);
                     csv.WriteField(jpk.PelnaNazwa);
                     csv.WriteField(jpk.REGON);
-                    for (int i = 0; i < fields.Count() - fields.ToList().IndexOf("REGON"); i++) csv.WriteField("");
+                    for (int i = 0; i < fields.Count() - fields.ToList().IndexOf("Email"); i++) csv.WriteField("");
                     csv.NextRecord();
 
-                    for (int i = 0; i < fields.ToList().IndexOf("KodKraju"); i++) csv.WriteField("");
-                    csv.WriteField(jpk.KodKraju);
-                    csv.WriteField(jpk.Wojewodztwo);
-                    csv.WriteField(jpk.Powiat);
-                    csv.WriteField(jpk.Gmina);
-                    csv.WriteField(jpk.Ulica);
-                    csv.WriteField(jpk.NrDomu);
-                    csv.WriteField(jpk.NrLokalu);
-                    csv.WriteField(jpk.Miejscowosc);
-                    csv.WriteField(jpk.KodPocztowy);
-                    csv.WriteField(jpk.Poczta);
-                    for (int i = 0; i < fields.Count() - fields.ToList().IndexOf("Poczta"); i++) csv.WriteField("");
-                    csv.NextRecord();
+                    //for (int i = 0; i < fields.ToList().IndexOf("KodKraju"); i++) csv.WriteField("");
+                    //csv.WriteField(jpk.KodKraju);
+                    //csv.WriteField(jpk.Wojewodztwo);
+                    //csv.WriteField(jpk.Powiat);
+                    //csv.WriteField(jpk.Gmina);
+                    //csv.WriteField(jpk.Ulica);
+                    //csv.WriteField(jpk.NrDomu);
+                    //csv.WriteField(jpk.NrLokalu);
+                    //csv.WriteField(jpk.Miejscowosc);
+                    //csv.WriteField(jpk.KodPocztowy);
+                    //csv.WriteField(jpk.Poczta);
+                    //for (int i = 0; i < fields.Count() - fields.ToList().IndexOf("Poczta"); i++) csv.WriteField("");
+                    //csv.NextRecord();
                     FkRejSprzedazy(jpk, csv);
                     FkRejZakupow(jpk, csv);
                 }
@@ -82,7 +84,10 @@ namespace JPKVat.Models
 
         public void FkRejSprzedazy(Ijpk jpk,CsvHelper.CsvWriter csv)
         {
-            //typSprzedazy	LpSprzedazy	NrKontrahenta	NazwaKontrahenta	AdresKontrahenta	DowodSprzedazy	DataWystawienia	DataSprzedazy	K_10	K_11	K_12	K_13	K_14	K_15	K_16	K_17	K_18	K_19	K_20	K_21	K_22	K_23	K_24	K_25	K_26	K_27	K_28	K_29	K_30	K_31	K_32	K_33	K_34	K_35	K_36	K_37	K_38	K_39
+            //typSprzedazy	NrKontrahenta	NazwaKontrahenta	AdresKontrahenta	DowodSprzedazy	DataWystawienia	DataSprzedazy	K_10	K_11	K_12	K_13	K_14	K_15	K_16	K_17	K_18	K_19	K_20	K_21	K_22	K_23	K_24	K_25	K_26	K_27	K_28	K_29	K_30	K_31	K_32	K_33	K_34	K_35	K_36	K_37	K_38	K_39
+            //LpSprzedazy   NrKontrahenta;NazwaKontrahenta;AdresKontrahenta;DowodSprzedazy;DataWystawienia;DataSprzedazy;           K_10;K_11;K_12;K_13;K_14;K_15;K_16;K_17;K_18;K_19;K_20;K_21;K_22;K_23;K_24;K_25;K_26;K_27;K_28;K_29;K_30;K_31;K_32;K_33;K_34;K_35;K_36;K_37;K_38;K_39;
+
+
             var fields = header.Split(";".ToCharArray());
             IFXHelper ifx = new IFXHelper();
             DataTable dt = ifx.GetRejSprzedazy(jpk.ConnectionString, jpk.DataOd, jpk.DataDo);
@@ -91,8 +96,8 @@ namespace JPKVat.Models
             double vat = 0.0;
             foreach (DataRow dr in dt.Rows)
             {
-                for (int i = 0; i < fields.ToList().IndexOf("typSprzedazy"); i++) csv.WriteField("");
-                csv.WriteField("G");
+                for (int i = 0; i < fields.ToList().IndexOf("LpSprzedazy"); i++) csv.WriteField("");
+                //csv.WriteField("G");
                 csv.WriteField(++ICount);
                 csv.WriteField(dr["nip"].ToString().Trim().Length == 0 ? " ": dr["nip"].ToString().Trim());
                 csv.WriteField(dr["nazwa"].ToString().Trim());
@@ -120,7 +125,9 @@ namespace JPKVat.Models
 
         public void FkRejZakupow(Ijpk jpk, CsvHelper.CsvWriter csv)
         {
-            //typZakupu;LpZakupu;NrDostawcy;NazwaDostawcy;AdresDostawcy;DowodZakupu;DataZakupu;DataWplywu;K_43;K_44;K_45;K_46;K_47;K_48;K_49;K_50;LiczbaWierszyZakupow;PodatekNaliczony
+            //LpZakupu;NrDostawcy;NazwaDostawcy;AdresDostawcy;DowodZakupu;DataZakupu;DataWplywu;K_43;K_44;K_45;K_46;K_47;K_48;K_49;K_50;LiczbaWierszyZakupow;PodatekNaliczony
+            //LpZakupu;NrDostawcy;NazwaDostawcy;AdresDostawcy;DowodZakupu;DataZakupu;DataWplywu;K_43;K_44;K_45;K_46;K_47;K_48;K_49;K_50;LiczbaWierszyZakupow;PodatekNaliczony
+
             var fields = header.Split(";".ToCharArray());
             IFXHelper ifx = new IFXHelper();
             DataTable dt = ifx.GetRefZakupow(jpk.ConnectionString, jpk.DataOd, jpk.DataDo);
@@ -128,8 +135,8 @@ namespace JPKVat.Models
             double vat = 0.0;
             foreach (DataRow dr in dt.Rows)
             {
-                for (int i = 0; i < fields.ToList().IndexOf("typZakupu"); i++) csv.WriteField("");
-                csv.WriteField("G");
+                for (int i = 0; i < fields.ToList().IndexOf("LpZakupu"); i++) csv.WriteField("");
+                //csv.WriteField("G");
                 csv.WriteField(++ICount);
                 csv.WriteField(dr["nip"].ToString().Trim().Length == 0 ? " " : dr["nip"].ToString().Trim());
                 csv.WriteField(dr["nazwa"].ToString().Trim());
--- JPKVat/Models/UstawieniaJPK.cs
+++ JPKVat/Models/UstawieniaJPK.cs
@@ -12,9 +12,9 @@ namespace JPKVat.Models
         private void InicjacjaJPK(Ijpk jpk)
         {
             jpk.KodFormularza = "JPK_VAT";
-            jpk.kodSystemowy = "JPK_VAT (2)";
-            jpk.wersjaSchemy = "1-0";
-            jpk.WariantFormularza = "2";
+            jpk.kodSystemowy = "JPK_VAT (3)";
+            jpk.wersjaSchemy = "1-1";
+            jpk.WariantFormularza = "3";
             jpk.CelZlozenia = "1";
             DateTime lastdayofPreviousMoth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddDays(-1);
             jpk.DataOd =  new DateTime(lastdayofPreviousMoth.Year, lastdayofPreviousMoth.Month, 1);
@@ -24,7 +24,7 @@ namespace JPKVat.Models
             jpk.KodUrzedu= "2413";
             jpk.NIP= "6310202474";
             jpk.PelnaNazwa= "DIGITAL SYSTEMS TECHNOLOGIES";
-            jpk.REGON= "003466199";
+            jpk.REGON= "ast@digisys-technologies.com";
             jpk.KodKraju ="PL";
             jpk.Wojewodztwo = "ŚLĄSKIE";
             jpk.Powiat = "GLIWICE";
